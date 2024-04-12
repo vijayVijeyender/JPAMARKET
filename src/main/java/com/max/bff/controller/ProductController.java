7f62099604bdbc46e4a30593a1e214af5debf936
vijeyender <vijayvijeyender@gmail.com>
2024-04-12 23:09:38 +0530

    Technical Exception

--- src/main/java/com/max/bff/controller/ProductController.java
+++ src/main/java/com/max/bff/controller/ProductController.java
@@ -4,9 +4,12 @@ import com.max.bff.dto.ProductDto;
 import com.max.bff.entity.Product;
 import com.max.bff.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+
+import static org.springframework.http.ResponseEntity.ok;
 
 @RestController
 @CrossOrigin(origins = "*", allowedHeaders = "*")
@@ -19,8 +22,9 @@ public class ProductController {
     public List<Product> getmethod(){
         return productService.getMethod();
     }
+
     @PostMapping("/createProduct")
-    public void saveProduct(@RequestBody ProductDto productDto){
-        productService.saveProduct(productDto);
+    public ResponseEntity<String> saveProduct(@RequestBody ProductDto productDto){
+        return ok(productService.saveProduct(productDto));
     }
 }
--- src/main/java/com/max/bff/service/BrandService.java
+++ src/main/java/com/max/bff/service/BrandService.java
@@ -1,11 +1,11 @@
 package com.max.bff.service;
 
-import com.max.bff.dto.BrandDto;
 import com.max.bff.entity.Brand;
-import com.max.bff.exception.AlreadyExistException;
+import com.max.bff.exception.TechnicalException;
 import com.max.bff.mapper.BrandMapper;
 import com.max.bff.repository.BrandRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -18,12 +18,11 @@ public class BrandService {
      BrandMapper brandMapper;
 
     public Brand saveBrand(Brand brand) {
-        try {
-            return brandRepository.save(brand);
-        }
-        catch (Exception e){
-            throw new AlreadyExistException("Brand Already Exist");
-        }
+try {
+    return brandRepository.save(brand);
+}catch (Exception e) {
+    throw new TechnicalException("Brand Already Exist", HttpStatus.CONFLICT);
+}
     }
 
     public List<Brand> getBrand() {
